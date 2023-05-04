65fe211435eac00824b317e7483fbb29aa05a7d1
LucsTx <lucstxx@gmail.com>
2023-05-04 16:58:03 -0300

    Exercicio de final de Módulo - Lucas
    Criar funçoes que realizem todas as operações aprendidas, e chamar essas funçoes na tela principal.

---
+++ funcoes.py
@@ -0,0 +1,28 @@
+
+def fimDoPrograma ():
+    print ('Fim do programa, obrigado!!')
+
+def soma (num1, num2):
+    somatoria = num1 + num2
+    return print('O resultado da soma e', somatoria) 
+
+def subtracao (num1, num2):
+    subtrai = num1 - num2
+    return print ('O resultado da subtracao e', subtrai)
+
+def multiplicacao (num1, num2):
+    multiplica = num1 * num2
+    return print ('O resultado da multiplicacao e', multiplica)
+
+def divisao (num1, num2):
+    divide = num1 / num2
+    return print ('O resultado da divisao e {:.2f}'.format (divide))
+
+def resDivisao (num1, num2):
+    resto = num1 % num2 
+    print ('O resto da divisao e', resto) 
+
+def divideInt (num1, num2):
+    resultInt = num1 // num2
+    print ('O resultado inteiro da divisao e', resultInt)
+
